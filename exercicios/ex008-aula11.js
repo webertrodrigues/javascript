b4c099198b5123539afd99df14a906a8143ab81f
Webert Rodrigues Caldeira da silva <129799974+webertrodrigues@users.noreply.github.com>
2024-04-26 19:37:02 -0300

    Atualização de exercícios aula 11

---
+++ exercicios/ex008-aula11.js
@@ -0,0 +1,6 @@
+var vel = 60
+console.log(`A velocidade de seu carro e de ${vel}Km/h`)
+if(vel > 60){ //condição simples
+    console.log(`Você utrapassou a velocidade permitida. MULTADO!!`)
+}
+console.log(`Dirija sem usando cinto de segurança!`)
---
+++ exercicios/ex009-aula11.js
@@ -0,0 +1,11 @@
+var país = "Brasil"
+console.log(`Vivendo em ${país}`)
+if(país ==  `Brasil`){
+    console.log(`Você é Brasileiro`)
+} else{
+    if(país == `EUA`){
+        console.log(`Você é Estadunidense`)
+    }else{
+        console.log(`Você é Estrangeiro`)
+    }
+}
