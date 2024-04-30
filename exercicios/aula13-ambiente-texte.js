307b3fe110f9099f71127080f1e74c8120cf5b16
Webert Rodrigues Caldeira da silva <129799974+webertrodrigues@users.noreply.github.com>
2024-04-30 10:37:10 -0300

    Criação de exercícios da aula 13 e 14

---
+++ exercicios/aula13-ambiente-texte.js
@@ -0,0 +1,20 @@
+var contador = 1
+do {
+    console.log(`Passo ${contador}`)
+    contador++
+} while (contador <= 5)
+
+/*var contador = 1
+while (contador <= 5){
+    console.log(`Passo ${contador}`)
+    contador++
+}*/
+
+/*
+console.log(`Passo 1`)
+console.log(`Passo 2`)
+console.log(`Passo 3`)
+console.log(`Passo 4`)
+console.log(`Passo 5`)
+*/
+
